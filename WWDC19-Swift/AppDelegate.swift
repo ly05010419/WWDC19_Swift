6af4bd8190277b84bce6cbf2bef9d008a2d636b7
li yong <ly05010419@gmail.com>
2019-09-30 16:16:56 +0200

    verbessert

--- WWDC19-Swift/AppDelegate.swift
+++ WWDC19-Swift/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyAV5Q0d2sRiivl5VBGb7XCuI1J4m7Fhe7s")
+        
         return true
     }
 
