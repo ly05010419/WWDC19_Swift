3e7d219e1797a03f0f2be2de6191277b2020bf75
li yong <ly05010419@gmail.com>
2019-11-09 17:35:15 +0100

    verbessert

--- WWDC19-Swift/controller/LandmarkDetailController.swift
+++ WWDC19-Swift/controller/LandmarkDetailController.swift
@@ -18,23 +18,32 @@ class LandmarkDetailController: UIViewController,UIScrollViewDelegate {
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor.white
         self.title = model?.name;
+        
+        let scrollView =  UIScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height));
+        scrollView.showsVerticalScrollIndicator = false
+        
         let width = UIScreen.main.bounds.size.width
         mapView = MapView(frame: CGRect(x: 0, y: 0, width: width, height: 400),model:self.model)
-        self.view.addSubview(mapView!)
+        mapView?.isUserInteractionEnabled=false
+        
+        scrollView.addSubview(mapView!)
         
         let avatar = Avatar(frame: CGRect(x:0 , y: 300, width: 200, height: 200),imageName: model?.imageName)
         avatar.center.x = self.view.center.x
-        self.view.addSubview(avatar)
+        scrollView.addSubview(avatar)
         
         let nameLabel = UILabel(frame: CGRect(x:0 , y: 500, width: width, height: 30))
         nameLabel.textAlignment = .center
         nameLabel.text = model?.name
         nameLabel.font = UIFont.systemFont(ofSize: 15.0)
-        self.view.addSubview(nameLabel)
+        scrollView.addSubview(nameLabel)
         
         let landmarkDetail:LandmarkDetail = LandmarkDetail(frame:
             CGRect(x:0 , y: 550, width:width , height: 320),dataList: dataList,controller: nil)
-        self.view.addSubview(landmarkDetail)
+        scrollView.addSubview(landmarkDetail)
+        
+        scrollView.contentSize = CGSize(width: width, height: 870)
+        self.view.addSubview(scrollView);
     }
 }
 
